3657c12b6ff4abba0d834bbd0f243905ade90a30
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-17 17:12:23 +0100

    Added tests

--- testing/test_main.py
+++ testing/test_main.py
@@ -12,19 +12,27 @@ import unittest
 from main import *
 
 class check_class : 
-   def first_arg(n) :
-      low, mean, up = limit(n)
+   def first_arg(n,m) :
+      low, mean, up = limit(n,m)
       return (( low - mean) / scipy.stats.norm.ppf(0.05) )**2   
    
-   def second_arg(n) :
-      low, mean, up = limit(n)
+   def second_arg(n,m) :
+      low, mean, up = limit(n,m)
       return mean 
    
-   def third_arg(n) :
-      low, mean, up = limit(n)
+   def third_arg(n,m) :
+      low, mean, up = limit(n,m)
       return (( up - mean) / scipy.stats.norm.ppf(0.95) )**2 
 
 class UnitTests(unittest.TestCase) :
+   def test_mean(self) :
+       inputs, var  = [], []
+       for i in range(2,10) :
+           inputs.append((i,))
+           rv = randomvar( 0.5, variance=1/12/i, vmin=0, vmax=1, isinteger=False )
+           var.append(rv)
+       assert( check_func("sample_mean", inputs, var ) )
+
    def test_exists(self) : 
        assert( exists("limit") )
 
